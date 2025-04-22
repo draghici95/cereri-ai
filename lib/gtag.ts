adcd62ccf3014d5d3a63fe456a0f3c267ff1592d
draghici95 <draghici95@yahoo.com>
2025-04-22 17:22:24 +0300

    Adăugat folder lib și tracking GA4

---
+++ lib/gtag.ts
@@ -0,0 +1,15 @@
+declare global {
+    interface Window {
+      gtag: (...args: any[]) => void;
+    }
+  }
+  
+  export const GA_TRACKING_ID = 'G-BZH2CDNGKD';
+  
+  export const pageview = (url: string) => {
+    window.gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+      debug_mode: true,
+    });
+  };
+  
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,42 @@
-// pages/_app.tsx
 import '@/styles/globals.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import Layout from '../components/Layout';
+import { GA_TRACKING_ID } from '../../lib/gtag';
 
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* Script Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){ dataLayer.push(arguments); }
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', { debug_mode: true });
+          `,
+        }}
+      />
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
